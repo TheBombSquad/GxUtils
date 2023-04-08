f9a791d93e83c29bbc32e73d6c143dd438566e83
The BombSquad <35253266+TheBombSquad@users.noreply.github.com>
2023-04-08 14:59:32 -0400

    Add conditional compilation to fix Mono build

--- GxUtils/GxModelViewer/Program.cs
+++ GxUtils/GxModelViewer/Program.cs
@@ -10,11 +10,13 @@ namespace GxModelViewer
 
     class MainClass
 	{
+    #if !__MonoCS__
         [DllImport("kernel32.dll")]
         private static extern IntPtr GetConsoleWindow();
 
         [DllImport("user32.dll")]
         private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    #endif
 
         private const int SW_HIDE = 0;
         private const int SW_SHOW = 5;
@@ -50,8 +52,10 @@ namespace GxModelViewer
             
             if (args.Length == 0)
             {
+                #if !__MonoCS__
                 var handle = GetConsoleWindow();
                 ShowWindow(handle, SW_HIDE);
+                #endif
                 Application.Run(modelViewer);
             }
             else
